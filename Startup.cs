4ba9618fa7cdd477851d35dd738dcc96641263a9
bouchaib-massioui <massioui.bouchaib@uit.ac.ma>
2020-08-29 16:06:55 +0100

    integrate swagger OPEN API

--- Startup.cs
+++ Startup.cs
@@ -1,3 +1,6 @@
+using System;
+using System.IO;
+using System.Reflection;
 using AutoMapper;
 using CommanderApi.Data;
 using Microsoft.AspNetCore.Builder;
@@ -6,6 +9,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
 using Newtonsoft.Json.Serialization;
 
 namespace CommanderApi
@@ -28,6 +32,35 @@ namespace CommanderApi
             // Force system to accept only the JSON Properties that match the setting
             services.AddControllers()
                     .AddNewtonsoftJson(s => { s.SerializerSettings.ContractResolver = new CamelCasePropertyNamesContractResolver(); });
+
+            // Register the Swagger generator, defining 1 or more Swagger documents
+            services.AddSwaggerGen(c =>
+            {
+                // c.SwaggerDoc("V1.0", new OpenApiInfo
+                // {
+                //     Version = "V1.0",
+                //     Title = "Commander API",
+                //     Description = "A simple Commander ASP.NET Core Web API",
+                //     TermsOfService = new Uri("https://example.com/terms"),
+                //     Contact = new OpenApiContact
+                //     {
+                //         Name = "Bouchaib MASSIOUI",
+                //         Email = string.Empty,
+                //         Url = new Uri("https://twitter.com/BMassioui"),
+                //     },
+                //     License = new OpenApiLicense
+                //     {
+                //         Name = "Use under LICX",
+                //         Url = new Uri("https://example.com/license"),
+                //     }
+                // });
+
+                // Set the comments path for the Swagger JSON and UI.
+                // need to enabled to generate xml file firstly
+                // var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                // var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+                // c.IncludeXmlComments(xmlPath);
+            });
 
             // DI
             /*
@@ -52,6 +85,18 @@ namespace CommanderApi
             }
 
             app.UseHttpsRedirection();
+
+            // Enable middleware to serve generated Swagger as a JSON endpoint.
+            app.UseSwagger();
+
+
+            // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+            // specifying the Swagger JSON endpoint.
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Commander API V1.0");
+                // c.RoutePrefix = string.Empty;
+            });
 
             app.UseRouting();
 
